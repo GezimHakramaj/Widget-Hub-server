cc702e44847502013331fb0eb253852068a7aee4
Gezim Hakramaj <gezimhakramaj@gmail.com>
2021-03-10 22:37:57 -0500

    Update port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(session({ secret: "Shh, its a secret!" }));
 
 app.use("/", require("./api"));
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Listening on port 8080.");
 });
 
